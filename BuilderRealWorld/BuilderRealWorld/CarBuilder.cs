157a1d96c4ce5a91243298c2727adea0e6f14e79
Juan Pablo Barrera Ospina <juan_barrerao@satrack.com>
2019-07-17 14:21:33 -0500

    creation project demo builder RealWorld

---
+++ BuilderRealWorld/BuilderRealWorld/CarBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderRealWorld
+{
+    class CarBuilder : VehicleBuilder
+    {
+        public CarBuilder()
+        {
+            vehicle = new Vehicle("Car");
+        }
+
+        public override void BuildDoors()
+        {
+            vehicle["doors"] = "4";
+        }
+
+        public override void BuildEngine()
+        {
+            vehicle["engine"] = "2500 cc";
+        }
+
+        public override void BuildFrame()
+        {
+            vehicle["frame"] = "Car Frame";
+        }
+
+        public override void BuildWheels()
+        {
+            vehicle["wheels"] = "4";
+        }
+    }
+}
---
+++ BuilderRealWorld/BuilderRealWorld/ScooterBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderRealWorld
+{
+    class ScooterBuilder : VehicleBuilder
+    {
+        public ScooterBuilder()
+        {
+            vehicle = new Vehicle("Scooter");
+        }
+
+        public override void BuildDoors()
+        {
+            vehicle["doors"] = "0";
+        }
+
+        public override void BuildEngine()
+        {
+            vehicle["engine"] = "50 cc";
+        }
+
+        public override void BuildFrame()
+        {
+            vehicle["frame"] = "Scooter Frame";
+        }
+
+        public override void BuildWheels()
+        {
+            vehicle["wheels"] = "2";
+        }
+    }
+}
